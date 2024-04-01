6443499fdec984480196df2a2c52302527ffd9bf
Phuong Quyen <pquyen245@gmail.com>
2024-04-01 11:17:54 +0700

    Squashed commit of the following:
    commit a68944adee9c9c43899bcaf1f618578cf2b5726b
    Author: Phuong Quyen <pquyen245@gmail.com>
    Date:   Mon Apr 1 10:35:08 2024 +0700
    
        add instruction for later reusing code
    
    commit 4f3f308dc44b3de8384b7c258b82c7fbd7673b84
    Author: Phuong Quyen <pquyen245@gmail.com>
    Date:   Sun Mar 31 23:42:29 2024 +0700
    
        update navigation's icons
    
    commit af3a17da50d04b76b2b0a3af617e3e495898d032
    Author: Phuong Quyen <pquyen245@gmail.com>
    Date:   Sun Mar 31 23:15:17 2024 +0700
    
        add top and bottom navigation

---
+++ app/src/main/java/com/example/foodorderingapp/customer_module/cart/cart.java
@@ -0,0 +1,17 @@
+package com.example.foodorderingapp.customer_module.cart;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorderingapp.R;
+
+public class cart extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cart);
+
+    }
+}
---
+++ app/src/main/java/com/example/foodorderingapp/customer_module/checkout/buy_success.java
@@ -0,0 +1,15 @@
+package com.example.foodorderingapp.customer_module.checkout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorderingapp.R;
+
+public class buy_success extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buy_success);
+    }
+}

70455360ffa93b76226c70df89ffdfbfd1cd1efc
Ngoc Anh <21520584@gm.uit.edu.vn>
2024-04-03 13:02:21 +0700

    UI AM-Navigation ver 2
    - thêm top-navigation
    - giảm cỡ chữ

--- app/src/main/java/com/example/foodorderingapp/UI/am_navigation.java
+++ app/src/main/java/com/example/foodorderingapp/UI/am_navigation.java
@@ -1,6 +1,7 @@
 package com.example.foodorderingapp.UI;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +16,10 @@ public class am_navigation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_am_navigation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView headerName = findViewById(R.id.screen_name);
+        headerName.setText("Tài khoản");
+
     }
 }

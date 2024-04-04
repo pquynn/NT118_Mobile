88d61a0005749da926e3dd6d8957fc3ba60961d2
Ngoc Anh <21520584@gm.uit.edu.vn>
2024-04-04 22:09:47 +0700

    UI Account Management Update ver 4
    Thêm màn hình:
    - User info
    - Change password
    - Change info

---
+++ app/src/main/java/com/example/foodorderingapp/am_change_userinfo.java
@@ -0,0 +1,21 @@
+package com.example.foodorderingapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class am_change_userinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_am_change_userinfo);
+        TextView headerName = findViewById(R.id.screen_name);
+        headerName.setText("Chỉnh sửa thông tin");
+    }
+}

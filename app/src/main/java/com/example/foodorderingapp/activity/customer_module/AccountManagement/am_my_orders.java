9f1cdc57604221579c164de3007fe28c9747602c
Ngoc Anh <21520584@gm.uit.edu.vn>
2024-04-08 15:06:07 +0700

    UI AccountManagement Point History
    - màn hình tích điểm

---
+++ app/src/main/java/com/example/foodorderingapp/activity/customer_module/AccountManagement/am_my_orders.java
@@ -0,0 +1,25 @@
+package com.example.foodorderingapp.activity.customer_module.AccountManagement;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.foodorderingapp.R;
+
+public class am_my_orders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_am_my_orders);
+
+        TextView headerName = findViewById(R.id.screen_name);
+        headerName.setText("Đơn hàng của tôi");
+
+    }
+}

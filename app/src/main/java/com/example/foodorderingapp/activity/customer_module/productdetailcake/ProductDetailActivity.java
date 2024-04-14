9d372f8cb34e1201f1660dfa174d18921cebd5f4
MyTamSW <21522569@gm.uit.edu.vn>
2024-04-14 20:21:04 +0700

    UI product detail cake
    add image, text, bottom add to cart view

---
+++ app/src/main/java/com/example/foodorderingapp/activity/customer_module/productdetailcake/ProductDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodorderingapp.activity.customer_module.productdetailcake;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorderingapp.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetail_cake);
+    }
+}

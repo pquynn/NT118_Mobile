5367ca1baf07f981a29d009333696092e1afa7b2
VTrung19 <100349884+VTrung19@users.noreply.github.com>
2024-04-17 00:13:58 +0700

    update admin order

--- app/src/main/java/com/example/foodorderingapp/Admin/AdminFragment/AdminOrder.java
+++ app/src/main/java/com/example/foodorderingapp/Admin/AdminFragment/AdminOrder.java
@@ -4,30 +4,52 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.foodorderingapp.R;
-import com.example.foodorderingapp.adapter.AdminOrderAdapter;
+import com.example.foodorderingapp.ViewPagerAdmin.AdminOrderViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AdminOrder extends Fragment {
-    private RecyclerView recyclerViewList;
-    private AdminOrderAdapter adapter;
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private AdminOrderViewPagerAdapter viewPagerAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_admin_order, container, false);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
-        recyclerViewList = view.findViewById(R.id.recyclerAdminOrderView);
-        recyclerViewList.setLayoutManager(linearLayoutManager);
+        TextView screenName = view.findViewById(R.id.screen_name);
+        screenName.setText("QUẢN LÝ ĐƠN HÀNG");
 
-        adapter = new AdminOrderAdapter();
-        recyclerViewList.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+
+        viewPagerAdapter = new AdminOrderViewPagerAdapter(getActivity());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, i) -> {
+            switch (i) {
+                case 0:
+                    tab.setText("Mới");
+                    break;
+                case 1:
+                    tab.setText("Đang giao");
+                    break;
+                case 2:
+                    tab.setText("Đã giao");
+                    break;
+                case 3:
+                    tab.setText("Đã hủy");
+                    break;
+            }
+        }).attach();
 
         return view;
     }

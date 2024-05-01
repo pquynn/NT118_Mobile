14a15a5d12a9f263ae23b83f4ec1d51dd70352bf
MyTamSW <21522569@gm.uit.edu.vn>
2024-05-01 22:45:34 +0700

    repository product - category

---
+++ app/src/main/java/com/example/foodorderingapp/Data/Repository/Category/CategoryRepository.java
@@ -0,0 +1,64 @@
+package com.example.foodorderingapp.Data.Repository.Category;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.foodorderingapp.Data.Model.Entity.Category;
+import com.example.foodorderingapp.Data.Model.Entity.Product;
+import com.example.foodorderingapp.Data.Model.ProductSearch;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryRepository implements ICategoryRepository{
+    private FirebaseFirestore db;
+    public CategoryRepository() {
+        db = FirebaseFirestore.getInstance();
+    }
+    @Override
+    public void getCategoryById(String categoryId, CategoryCallBack callBack) {
+        DocumentReference categoryRef = db.collection("CATEGORY").document(categoryId);
+        categoryRef.get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        Category category = documentSnapshot.toObject(Category.class);
+                        callBack.onCategoryLoaded(category);
+                    } else {
+                        callBack.onCategoryLoadFailed("Category not found");
+                    }
+                })
+                .addOnFailureListener(e -> callBack.onCategoryLoadFailed(e.getMessage()));
+    }
+
+    //lấy ảnh và tên của category
+    public void getListCategory(CategoryListCallBack callBack) {
+        db.collection("CATEGORY")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<Category> categoryList = new ArrayList<>();
+                        for (DocumentSnapshot document : task.getResult()) {
+                            String id = document.getId();
+                            String categoryImage = document.getString("CATEGORY_IMAGE");
+                            String categoryName = document.getString("CATEGORY_NAME");
+
+                            Category category = new Category(categoryImage, categoryName);
+                            categoryList.add(category);
+                        }
+                        callBack.onCategoryListLoaded(categoryList);
+                    } else {
+                        Log.e("TAG", "Error getting documents: ", task.getException());
+                        callBack.onCategoryListLoadFailed(task.getException());
+                    }
+                });
+    }
+}
+

6a11bdbf47716a74f24e1c74a9f13277f45fc4dd
Ngoc Anh <21520584@gm.uit.edu.vn>
2024-05-08 21:37:05 +0700

    complete viewmodel - view my orders by status

---
+++ app/src/main/java/com/example/foodorderingapp/ui/viewmodel/customer/accountmanagement/MyOrdersVM.java
@@ -0,0 +1,36 @@
+package com.example.foodorderingapp.ui.viewmodel.customer.accountmanagement;
+
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.foodorderingapp.data.model.entity.Order;
+import com.example.foodorderingapp.data.repository.accountmanagement.myorders.OrderListRepository;
+import com.example.foodorderingapp.data.repository.order.IOrderRepository;
+import com.example.foodorderingapp.data.repository.order.OrderRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyOrdersVM extends ViewModel {
+    OrderRepository repository = new OrderRepository();
+    MutableLiveData<List<Order>> orderListLiveData = new MutableLiveData<>();
+    public MyOrdersVM (String userId, String orderStatus){
+        repository.getOrderListByStatusAndUserId(userId, orderStatus, new IOrderRepository.OrderListCallback() {
+            @Override
+            public void onOrderListLoaded(List<Order> orderList) {
+                orderListLiveData.setValue(orderList);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                Log.e("MyOrdersVMError", "Error load my orders by status in MyOrdersVM");
+            }
+        });
+    }
+
+    public MutableLiveData<List<Order>> getOrderListLiveData(){
+        return orderListLiveData;
+    }
+}

7ebe46ea68be1aae0b7b630d9972ecb68312de93
Phuong Quyen <pquyen245@gmail.com>
2024-05-10 02:33:31 +0700

    update refund ui
    add btn select video

--- app/src/main/java/com/example/foodorderingapp/ui/activityfragment/customer/refund/AddRefundProductActivity.java
+++ app/src/main/java/com/example/foodorderingapp/ui/activityfragment/customer/refund/AddRefundProductActivity.java
@@ -47,8 +47,8 @@ public class AddRefundProductActivity extends AppCompatActivity {
 
         ArrayList<OrderDetail> productList = new ArrayList<OrderDetail>();
 
-//        productList.add(new OrderDetail("Trà sữa trân châu", 4000, "Lớn", "50% đường", 3));
-//        productList.add(new OrderDetail("Bánh", 60000, "Lớn", "a", 2));
+        productList.add(new OrderDetail("Trà sữa trân châu", 45000, "Lớn", "50% đường", 3, ""));
+        productList.add(new OrderDetail("Trà sữa trân châu", 45000, "Lớn", "50% đường", 3, ""));
 
         adapter = new RefundProductAdapter(productList);
         recyclerViewList.setAdapter(adapter);

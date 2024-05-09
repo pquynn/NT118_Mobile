07eeb5ca8cde8cf3d65d04f3c2a293d452ccca43
MyTamSW <21522569@gm.uit.edu.vn>
2024-05-10 00:22:42 +0700

    update ProductReponsitory, entity Product, ProductDetailCakeActivity

--- app/src/main/java/com/example/foodorderingapp/data/model/entity/Product.java
+++ app/src/main/java/com/example/foodorderingapp/data/model/entity/Product.java
@@ -1,19 +1,25 @@
 package com.example.foodorderingapp.data.model.entity;
 
+import com.google.firebase.firestore.DocumentId;
 import com.google.firebase.firestore.PropertyName;
+import com.google.firebase.firestore.ServerTimestamp;
 
 import java.util.List;
 import java.util.Map;
 
 public class Product {
+    @DocumentId
     private String id;
     private String productImage;
     private String idCategory;
     private String productName;
     private int productPrice;
     private String productInfo;
-    private Map<String, Map<String, Object>> productSize;
+    @ServerTimestamp
+    private Map<String, Map<String, Integer>> productSize;
     private List<String> topping;
+
+    public Product() {}
 
     //constructor lấy id, idCategory, image, name, price
     public Product(String id, String idCategory, String productImage, String productName, int productPrice) {
@@ -33,7 +39,7 @@ public class Product {
         this.productInfo = productInfo;
     }
 
-    public Product(String id, String productImage, String idCategory, String productName, int productPrice, String productInfo, List<String> topping, Map<String, Map<String, Object>> productSize) {
+    public Product(String id, String productImage, String idCategory, String productName, int productPrice, String productInfo, List<String> topping, Map<String, Map<String, Integer>> productSize) {
         this.id = id;
         this.productImage = productImage;
         this.idCategory = idCategory;
@@ -43,81 +49,68 @@ public class Product {
         this.topping = topping;
         this.productSize = productSize;
     }
-    @PropertyName("ID_PRODUCT")
+
     public String getId() {
         return id;
     }
 
-    @PropertyName("ID_PRODUCT")
     public void setId(String id) {
         this.id = id;
     }
-
     @PropertyName("PRODUCT_IMAGE")
     public String getProductImage() {
         return productImage;
     }
-
     @PropertyName("PRODUCT_IMAGE")
     public void setProductImage(String productImage) {
         this.productImage = productImage;
     }
-
-    @PropertyName("ID_CATEGORY")
+    @PropertyName("ID_PRODUCT")
     public String getIdCategory() {
         return idCategory;
     }
-
-    @PropertyName("ID_CATEGORY")
+    @PropertyName("ID_PRODUCT")
     public void setIdCategory(String idCategory) {
         this.idCategory = idCategory;
     }
-
     @PropertyName("PRODUCT_NAME")
     public String getProductName() {
         return productName;
     }
-
     @PropertyName("PRODUCT_NAME")
     public void setProductName(String productName) {
         this.productName = productName;
     }
-
     @PropertyName("PRODUCT_PRICE")
     public int getProductPrice() {
         return productPrice;
     }
-
     @PropertyName("PRODUCT_PRICE")
     public void setProductPrice(int productPrice) {
         this.productPrice = productPrice;
     }
-
     @PropertyName("PRODUCT_INFO")
     public String getProductInfo() {
         return productInfo;
     }
-
     @PropertyName("PRODUCT_INFO")
     public void setProductInfo(String productInfo) {
         this.productInfo = productInfo;
     }
-
     @PropertyName("TOPPING")
     public List<String> getTopping() {
         return topping;
     }
-
     @PropertyName("TOPPING")
     public void setTopping(List<String> topping) {
         this.topping = topping;
     }
-
-    public Map<String, Map<String, Object>> getProductSize() {
+    @PropertyName("SIZE")
+    public Map<String, Map<String, Integer>> getProductSize() {
         return productSize;
     }
-
-    public void setProductSize(Map<String, Map<String, Object>> productSize) {
+    @PropertyName("SIZE")
+    public void setProductSize(Map<String, Map<String, Integer>> productSize) {
         this.productSize = productSize;
     }
 
